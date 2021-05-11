9df73044e50214acfad4ce3774e1da4a08e6ec05
michaelJwilson <mjwilson@lbl.gov>
2021-05-11 15:41:43 -0700

    Normalize to GFA times.

--- plot-extfiberloss.py
+++ plot-extfiberloss.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from   scipy import stats
-from   astropy.table import Table, vstack
+from   astropy.table import Table, vstack, join
 from   desispec.io.meta import findfile
 
 # fname = '/global/cscratch1/sd/mjwilson/trash/aux_psf.pickle'
@@ -71,18 +71,36 @@ tables = tables['expid', 'tileid', 'night', 'program', 'airmass', 'atm_extinctio
                 'tsnr_seeing', 'tsnr_alpha']
 
 tables.sort('tsnr2_spec_r')
+
+daily  = Table.read('/project/projectdirs/desi/spectro/redux/daily/tsnr-exposures.fits', 1)
+daily  = Table(daily[daily['NIGHT'] == 20210510])
+daily  = daily['EXPID', 'SKY_MAG_AB_GFA', 'SKY_MAG_G_SPEC', 'SKY_MAG_R_SPEC', 'SKY_MAG_Z_SPEC', 'EFFTIME_GFA', 'EFFTIME_DARK_GFA', 'EFFTIME_BRIGHT_GFA', 'EFFTIME_BACKUP_GFA']
+
+keys   = list(daily.dtype.names) 
+
+for x in keys:
+    # print(x)
+
+    daily[x.lower()] = daily[x.upper()]
+    
+    del daily[x]
+
+daily.pprint()
+    
+tables = join(tables, daily, join_type='left', keys='expid')
 tables.pprint()
 
 # del tables['nominal_seeing_fwhm']
 
 tables.write(fname.replace('.pickle', '.fits'), format='fits', overwrite=True)
 
+#
 fig, axes = plt.subplots(1, 2, figsize=(10,5))
 
 for i, (marker, program) in enumerate(zip(['^', '*'], ['DARK', 'BRIGHT'])):
     isin = tables['program'] == program
     
-    gradient=np.sum(tables['tsnr2_spec_r'][isin] * tables['efftime'][isin]) / np.sum(tables['tsnr2_spec_r'][isin]**2.)
+    gradient=np.sum(tables['tsnr2_spec_r'][isin] * tables['efftime_gfa'][isin]) / np.sum(tables['tsnr2_spec_r'][isin]**2.)
         
     axes[i].scatter(tables['tsnr2_spec_r'][isin], tables['efftime'][isin] / (gradient * tables['tsnr2_spec_r'][isin]), marker=marker, lw=0.0, s=14)
     axes[i].set_xlabel('TSNR2_SPEC_R')
@@ -91,11 +109,12 @@ for i, (marker, program) in enumerate(zip(['^', '*'], ['DARK', 'BRIGHT'])):
     
 pl.show() 
 pl.clf()
+
 '''
 for i, (marker, program) in enumerate(zip(['^', '*'], ['DARK', 'BRIGHT'])):
     isin = tables['program'] == program
 
-    gradient=np.sum(tables['tsnr2_spec_r'][isin] * tables['efftime'][isin]) / np.sum(tables['tsnr2_spec_r'][isin]**2.)
+    gradient=np.sum(tables['tsnr2_spec_r'][isin] * tables['efftime_gfa'][isin]) / np.sum(tables['tsnr2_spec_r'][isin]**2.)
     
     axes[i].hist(tables['efftime'][isin] / (gradient * tables['tsnr2_spec_r'][isin]), histtype='step', bins=np.arange(0.7, 1.3, 0.05))
 
